ee43cc4b264467634188dc03ab0f92ea9cc5df35
dogiahuy2002 <dogiahuy1407@gmail.com>
2024-11-13 09:01:06 +0700

    complete interface - Huy

--- BTL_Nhom14_ECommercial/screens/ProductDetail2.js
+++ BTL_Nhom14_ECommercial/screens/ProductDetail2.js
@@ -29,38 +29,23 @@ const data1 =[
   ]
 
 export default function ProductDetail2({navigation}){
-    // const renderItem = ({item})=>(
-    //     <Pressable>
-    //         <View style={styles.item}>
-    //             <View style={{flex:6, justifyContent:"center", alignItems:"center"}}>
-    //                 <Image
-    //                     source={{uri:item.image}}
-    //                     style={{width:70, height:70}}
-    //                 />
-    //             </View>
-    //             <View style={{flex:2, justifyContent:"center", marginLeft:20}}>
-    //                 <Text>
-    //                     {item.title}
-    //                 </Text>
-    //             </View>
-    //             <View style={{flex:2, flexDirection:"row", alignItems:"center"}}>
-    //                 <Image
-    //                     source={require("../images/star.png")}
-    //                     style={{width:20, height:20, marginLeft:20}}
-    //                 />
-    //                 <Text style={{fontSize:13, marginLeft:5}}>
-    //                     4.5
-    //                 </Text>
-    //                 <Text style={styles.price1}>
-    //                     {item.price}
-    //                 </Text>
-    //             </View>
-    //         </View>
-    //     </Pressable>
-    // )
+    //useState tăng giảm sô lượng 
+    const [count, setCount] = useState(1);
+    //useState giá, thành tiền
+    const [price, setTotal] = useState(2.99);
+    const increment = () => {
+        setCount(count + 1);
+    };
+    const decrement = () => {
+        setCount(count - 1);
+    };
+    //chọn size
+    const [selectedSize, setSelectedSize] = useState(null);
+
+    const sizes = ['XS','S', 'M', 'L', 'XL'];
     return (
         <ScrollView>
-            <View style={{flex:1,width:360, height:1050, backgroundColor:"#FFFFFF" }}>
+            <View style={{flex:1, height:1150, backgroundColor:"#FFFFFF" }}>
                 <View style={{flex:1}}></View>
                 <View style={{flex:1, flexDirection: 'row', alignItems:'center'}}>
                     <Pressable onPress={()=>navigation.navigate('Home_ProductListing')}>
@@ -80,14 +65,14 @@ export default function ProductDetail2({navigation}){
                 <View style={{flex:2, flexDirection:"row"}}>
                         <Image
                             source={{uri:"https://plus.unsplash.com/premium_photo-1673827311290-d435f481152e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aG9vZGllfGVufDB8fDB8fHww"}}
-                            style={{width:72.5, height:72.5, borderRadius:6,marginLeft:20}}
+                            style={{width:72.5, height:72.5, borderRadius:6,marginLeft:35}}
                         />
                         <Image
                             source={{uri:"https://images.unsplash.com/photo-1542406775-ade58c52d2e4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGhvb2RpZXxlbnwwfHwwfHx8MA%3D%3D"}}
                             style={{width:72.5, height:72.5, borderRadius:6,marginLeft:10}}
                         />
                         <Image
-                            source={{uri:"https://media.istockphoto.com/id/2141442585/fr/photo/african-american-man-with-african-hairstyle-wearing-hoodie-standing-over-isolated-yellow.webp?a=1&b=1&s=612x612&w=0&k=20&c=erAsWKyEQxuLZiW23QEF5EzZBLcEhahot6pjDTaebJA="}}
+                            source={{uri:"https://media.istockphoto.com/id/2170496889/vi/anh/african-american-man-with-african-hairstyle-wearing-hoodie-standing-over-isolated-yellow.jpg?s=612x612&w=0&k=20&c=Xd3ChKIF6UCICBLQd43sRgEKeQ-uM_wXH44qwOeaHfU="}}
                             style={{width:72.5, height:72.5, borderRadius:6,marginLeft:10}}
                         />
                         <Image
@@ -96,13 +81,13 @@ export default function ProductDetail2({navigation}){
                         />
                 </View>
                 <View style={{flex:10}}>
-                    <View style={{flex:2, flexDirection:"row", alignItems:"center"}}>
+                    <View style={{flex:1.5, flexDirection:"row", alignItems:"center"}}>
                         <Text style={styles.price}>$2,99</Text>
                         <Pressable style={styles.press1}>
                             <Text style={styles.txt1}>Buy 1 get 1</Text>
                         </Pressable>
                     </View>
-                    <View style={{flex:1, flexDirection:"row", alignItems:"center"}}>
+                    <View style={{flex:1.5, flexDirection:"row", alignItems:"center"}}>
                         <Text style={styles.txt2}>Hoodie shirt</Text>
                         <Image
                                 source={require('../images/star.png')}
@@ -111,11 +96,96 @@ export default function ProductDetail2({navigation}){
                             <Text style={{marginLeft:5}}>4.5</Text>
                     </View>
                     <View style={{flex:1}}>
-                        <Text style={styles.txt3}>zzzzzz</Text>
+                        <Text style={styles.txt3}>African american man with african hairstyle wearing hoodie standing over isolated yellow background</Text>
                     </View>
-                    <View style={{flex:6}}></View>
+                    <View style={{flex:0.5}}></View>
+                    <View style={{flex:8}}>
+                        <View style={{flex:2}}>
+                            <Text style={styles.size}>Color</Text>
+                            <View style={{flex:1, flexDirection:"row", alignItems:"center"}}>
+                                <Pressable style={styles.color1}/>
+                                <Pressable style={styles.color2}/>
+                                <Pressable style={styles.color3}/>
+                            </View>
+                        </View>
+                        <View style={{flex:2}}>
+                            <Text style={styles.size}>Size</Text>
+                            <View style={{flex:1, flexDirection:"row", alignItems:"center",marginLeft:20}}>
+                            {sizes.map((size, index) => (
+                                <Pressable
+                                key={index}
+                                onPress={() => setSelectedSize(size)}
+                                style={[
+                                    styles.Buttsize,
+                                    selectedSize === size && styles.selectedButton,
+                                ]}
+                                >
+                                <Text>{size}</Text>
+                                </Pressable>
+                            ))}
+                                
+                            </View>
+                        </View>
+                        <View style={{flex:2, borderBottomWidth:1, borderColor:"#C5BCBC"}}>
+                            <Text style={styles.size}>Quantity</Text>
+                            <View style={{
+                                flex:2,
+                                flexDirection:"row", alignItems:"center"
+                            }}>
+                                <Pressable 
+                                onPress={decrement} 
+                                style={({ pressed }) => [
+                                    {
+                                    backgroundColor: pressed ? 'aqua' : 'lightgray',
+                                    },
+                                    styles.button,
+                                ]}
+                                >
+                                    <Text style={styles.text}>-</Text>
+                                </Pressable>
+                                <Text style={{fontSize: 20,fontWeight:700, marginLeft:20}}>{count}</Text>
+                                <Pressable 
+                                onPress={increment} 
+                                style={({ pressed }) => [
+                                    {
+                                    backgroundColor: pressed ? 'aqua' : 'lightgray',
+                                    },
+                                    styles.button,
+                                ]}
+                                >
+                                    <Text style={styles.text}>+</Text>
+                                </Pressable>
+                                <Text style={styles.total}>Total</Text>
+                                <Text style={styles.totalP}>$ {price*count}</Text>
+                            </View>
+                        </View>
+                    </View>
                 </View>
-                <View style={{flex:6}}></View>
+                <View style={{flex:6}}>
+                    <View style={{flex:2,borderBottomWidth:1, borderColor:"#C5BCBC", alignItems:"center", flexDirection:"row"}}>
+                        <Text style={styles.guide}>Size guide</Text>  
+                        <Image
+                            source={require("../images/than.png")}
+                            style={{width:30,height:30,marginLeft:140}}
+                        />     
+                    </View>
+                    <View style={{flex:2,borderBottomWidth:1, borderColor:"#C5BCBC", alignItems:"center", flexDirection:"row"}}>
+                        <Text style={styles.guide}>Review (99)</Text>  
+                        <Image
+                            source={require("../images/than.png")}
+                            style={{width:30,height:30,marginLeft:140}}
+                        />      
+                    </View>
+                    <View style={{flex:2, alignItems:"center", justifyContent:"center"}}>
+                        <Pressable style={styles.butt}>
+                            <Image
+                                source={require("../images/BCart.png")}
+                                style={{width:20,height:20, marginRight:4}}
+                            />
+                            <Text style={{color:"#FFFFFF"}}>Add to cart</Text>
+                        </Pressable>
+                    </View>
+                </View>
                 
                 
 
@@ -144,8 +214,8 @@ const styles = StyleSheet.create({
     txt3:{
         fontSize:14, fontWeight:"500", marginLeft:20, color:"#A4A9A8"
     },
-    see:{
-        fontSize:14, fontWeight:"500", marginLeft:80, marginTop:10, color:"#A4A9A8"
+    size:{
+        fontSize:18, fontWeight:"400", marginLeft:20, marginTop:10
     },
     name:{
         fontSize:14, fontWeight:"500", marginLeft:20, marginTop:5, color:"#A4A9A8"
@@ -154,13 +224,50 @@ const styles = StyleSheet.create({
         fontSize:12, fontWeight:"500", marginLeft:20, marginTop:5, color:"#A4A9A8"
     },
     butt:{
-        marginLeft:10, width:260, height:50, backgroundColor:"#11D5EB", borderRadius:5, justifyContent:"center", alignItems:"center"
+         width:300, height:50, backgroundColor:"#00BDD6", borderRadius:5, 
+         justifyContent:"center", alignItems:"center", flexDirection:"row"
     },
     txtBut:{
         color:"white",
         fontWeight:"600",
         textAlign:"center",
         fontSize:18
-    }
+    },
+    color1:{
+        width:30, height:30, borderRadius:40, backgroundColor:"red", borderWidth:2, borderColor:"#A19999", marginLeft:25
+    },
+    color2:{
+        width:30, height:30, borderRadius:40, backgroundColor:"brown", borderWidth:2, borderColor:"#A19999", marginLeft:25
+    },
+    color3:{
+        width:30, height:30, borderRadius:40, backgroundColor:"yellow", borderWidth:2, borderColor:"#A19999", marginLeft:25
+    },
+    Buttsize:{
+        width:40, height:40,borderRadius:5, alignItems:"center", justifyContent:"center", borderWidth:1, borderColor:"#A19999", marginRight:5
+    },
+    button: {
+        alignItems: 'center',
+        height:30,
+        width:30,
+        justifyContent:"center", 
+        alignItems:"center", marginLeft:20, borderRadius:5
+        
+      },
+      text: {
+        fontSize: 24,
+        fontWeight:"700"
+      },
+    total:{
+        color:"#A19999", fontSize:12, fontWeight:"500", marginLeft:120
+    },
+    totalP:{
+        fontSize:20, fontWeight:"700", marginLeft:10,
+    },
+    guide:{
+        fontSize:18, fontWeight:"700", marginLeft:20, width:150
+    },
+    selectedButton: {
+        backgroundColor: '#00BDD6', // Màu nền khi được chọn
+      },
     
 });
